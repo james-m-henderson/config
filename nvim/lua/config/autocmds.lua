df9492bb4b43eb8ebacaf306dbe7d3f5e7277aef
James Henderson <jamehend@cisco.com>
2023-08-18 15:44:08 -0400

    Automatically run black on files when writing

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,9 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = {"*.py"},
+  command = "!black %",
+})
