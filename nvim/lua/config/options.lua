d79904a5d577fb3e947e934ad63fc7bde11b3a08
James Henderson <james.henderson@dataductus.com>
2025-09-17 09:12:45 -0400

    Latest nvim setup

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,71 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+vim.opt.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = paste,
+    ["*"] = paste,
+    --["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    --["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
+vim.g.root_spec = { { ".git" }, "lsp", "cwd" }
+
+opts = function()
+  local plugin = require("lazy.core.config").plugins["conform.nvim"]
+  if plugin.config ~= M.setup then
+    LazyVim.error({
+      "Don't set `plugin.config` for `conform.nvim`.\n",
+      "This will break **LazyVim** formatting.\n",
+      "Please refer to the docs at https://www.lazyvim.org/plugins/formatting",
+    }, { title = "LazyVim" })
+  end
+  ---@type conform.setupOpts
+  local opts = {
+    default_format_opts = {
+      timeout_ms = 3000,
+      async = false, -- not recommended to change
+      quiet = false, -- not recommended to change
+      lsp_format = "fallback", -- not recommended to change
+    },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      fish = { "fish_indent" },
+      sh = { "shfmt" },
+    },
+    -- The options you set here will be merged with the builtin formatters.
+    -- You can also define any custom formatters here.
+    ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
+    formatters = {
+      injected = { options = { ignore_errors = true } },
+      -- # Example of using dprint only when a dprint.json file is present
+      -- dprint = {
+      --   condition = function(ctx)
+      --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+      --   end,
+      -- },
+      --
+      -- # Example of using shfmt with extra args
+      -- shfmt = {
+      --   prepend_args = { "-i", "2", "-ci" },
+      -- },
+    },
+  }
+  return opts
+end
