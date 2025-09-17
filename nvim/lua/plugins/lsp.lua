226bf866daf43ee0ea362637195e83431be9752d
James Henderson <james.henderson@dataductus.com>
2025-09-17 16:14:28 -0400

    Remove old lspconfig

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        underline = true,
+        update_in_insert = false,
+        virtual_text = {
+          spacing = 4,
+          source = "if_many",
+          prefix = "●",
+        },
+        severity_sort = true,
+      },
+      inlay_hints = { enabled = false },
+      format = {
+        formatting_options = nil,
+        timeout_ms = nil,
+      },
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              workspace = { checkThirdParty = false },
+              completion = { callSnippet = "Replace" },
+            },
+          },
+        },
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                pycodestyle = { maxLineLength = 200 },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
+
